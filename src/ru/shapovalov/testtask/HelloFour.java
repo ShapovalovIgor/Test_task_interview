3e08184dc0d081c1be89c90c95cf0b9d9676f8a7
ShapovalovIgor <shapovalov0010@gmail.com>
2018-01-16 09:50:09 +0400

    Add example

--- src/ru/shapovalov/testtask/HelloFour.java
+++ src/ru/shapovalov/testtask/HelloFour.java
@@ -7,11 +7,15 @@ import java.util.List;
 public class HelloFour {
     //Написать метод возвращабщий факториал
     public static void main(String args[]) {
-
+        int factorialList = factorial(3);
+        System.out.println(factorialList);
         BigInteger factorial = factorial(new BigInteger("3"));
         System.out.println(factorial.intValue());
     }
 
+    public static int factorial(int num) {
+        return (num == 0) ? 1 : num * factorial (num - 1);
+    }
 
     public static BigInteger factorial(BigInteger n) {
         BigInteger result = BigInteger.ONE;
