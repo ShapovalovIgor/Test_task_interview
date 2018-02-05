96003e3bb2802ec9fa735d38ba4bc909ce8b618a
ShapovalovIgor <shapovalov0010@gmail.com>
2018-02-05 17:03:40 +0400

    Refactoring interview code sleep

--- src/ru/shapovalov/example/ExampleEight.java
+++ src/ru/shapovalov/example/ExampleEight.java
@@ -17,7 +17,7 @@ public class ExampleEight implements Runnable {
         executorService.submit(firstTest);
         executorService.submit(secondTest);
         TimeUnit.SECONDS.sleep(3);
-        System.exit(0);
+        executorService.shutdown();
     }
 
     public ExampleEight(int i) {
