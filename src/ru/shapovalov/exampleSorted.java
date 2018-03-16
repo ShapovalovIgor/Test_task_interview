447c0777b236e539263c39f86b24563bc5e69961
Shapovalov <shapovalov0010@gmail.com>
2018-03-16 13:43:39 +0400

    Add code sorted

--- src/ru/shapovalov/exampleSorted.java
+++ src/ru/shapovalov/exampleSorted.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class exampleSorted {
     public static void main(String[] args) {
-        int[] arraySource = new int[99999];
+        int[] arraySource = new int[9];
         for (int index = 0; index < arraySource.length; index++) {
             Random random = new Random();
             arraySource[index] = random.nextInt();
@@ -27,6 +27,7 @@ public class exampleSorted {
         System.out.println("One: " + durationOne);
         System.out.println("Two: " + durationTwo);
         System.out.println("Three: " + durationThree);
+        System.out.println("Four" + Arrays.toString(quickSort(arraySource)));
 
     }
 
@@ -50,16 +51,39 @@ public class exampleSorted {
 
     // Временая сложность O(n)
     private static int[] bubbleSort(int[] array) {
-        for (int indexToScan = 0; indexToScan < array.length - 1; indexToScan++) {
-            int tmp = array[indexToScan];
-            if (array[indexToScan] > array[indexToScan + 1]) {
-                array[indexToScan] = array[indexToScan + 1];
-                array[indexToScan + 1] = tmp;
+        boolean swapped = true;
+
+        int j = 0;
+
+        int tmp;
+
+        while (swapped) {
+
+            swapped = false;
+
+            j++;
+
+            for (int i = 0; i < array.length - j; i++) {
+
+                if (array[i] > array[i + 1]) {
+
+                    tmp = array[i];
+
+                    array[i] = array[i + 1];
+
+                    array[i + 1] = tmp;
+
+                    swapped = true;
+
+                }
+
             }
+
         }
         return array;
     }
 
+    //Временая сложность O(n^2)
     private static int[] insertSort(int[] array) {
         int element;
         int indexToInsert;
@@ -74,5 +98,58 @@ public class exampleSorted {
             }
         }
         return array;
+    }
+
+    private static  int[]  quickSort(int[] array) {
+        int[] tmpArray = new int[]{};
+        tmpArray = array;
+return quickSort(tmpArray, 0, 0);
+    }
+    private static int[] quickSort(int[] array, int lowerIndex, int higherIndex) {
+        if (array == null || array.length == 0) {
+            return null;
+        }
+        if (lowerIndex == 0
+                && higherIndex == 0) {
+            lowerIndex = 0;
+            higherIndex = array.length - 1;
+        }
+        int i = lowerIndex;
+        int j = higherIndex;
+        // calculate pivot number, I am taking pivot as middle index number
+        int pivot = array[lowerIndex + (higherIndex - lowerIndex) / 2];
+        // Divide into two arrays
+        while (i <= j) {
+            /**
+             * In each iteration, we will identify a number from left side which
+             * is greater then the pivot value, and also we will identify a number
+             * from right side which is less then the pivot value. Once the search
+             * is done, then we exchange both numbers.
+             */
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                exchangeNumbers(array, i, j);
+                //move index to next position on both sides
+                i++;
+                j--;
+            }
+        }
+        // call quickSort() method recursively
+        if (lowerIndex < j)
+            quickSort(array, lowerIndex, j);
+        if (i < higherIndex)
+            quickSort(array, i, higherIndex);
+        return array;
+    }
+
+    private static void exchangeNumbers(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
