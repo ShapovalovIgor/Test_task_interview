cf83a790f5e84e703698986ca8a0ac8b7b3d4759
ShapovalovIgor <shapovalov0010@gmail.com>
2018-08-15 23:22:25 +0400

    Readme refactoring and add question

---
+++ src/ru/shapovalov/testtask/HelloSeven.java
@@ -0,0 +1,12 @@
+package ru.shapovalov.testtask;
+
+public class HelloSeven {
+    public static void main( String[] args ) {
+    String a = "1234567";
+    aaaa(a);
+    System.out.println(a);
+    }
+    public static void aaaa(String a){
+        a = "999";
+    }
+}
